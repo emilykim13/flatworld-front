80e96d57f38197a3e2e3ff5df173f270cad9e790
github username <emilyn0614@gmail.com>
2022-03-11 07:55:14 -0600

    add href link for chapters and front matter

--- src/Routes.js
+++ src/Routes.js
@@ -2,19 +2,18 @@ import React from "react"
 import { BrowserRouter as Router, Routes as Switch, Route, Redirect} from "react-router-dom"
 import ChaptersList from "./components/ChaptersList.js"
 import Chapter from "./components/Chapter.js"
-
 import { useSelector } from 'react-redux'
 import SectionsList from "./components/SectionsList.js"
+
+
 const Routes = (props) => {
 
     return(
         <Router>
             <Switch>
                 <Route path='/chapters' exact element = { <ChaptersList {...props} /> } />
-                {/* <Route path='/sections-list' exact element = { <SectionsList {...props} /> }/> */}
                 <Route path='/chapter_sections' exact element = { <SectionsList {...props} /> }/>
-
-                {/* <Route path='/chapter/:id/:type_one' exact element = { <Chapter {...props} /> }/> */}
+                <Route path='/chapter/:id/:type_one' exact element = { <Chapter {...props} /> }/>
             </Switch>
         </Router>
     )
--- src/components/Chapter.js
+++ src/components/Chapter.js
@@ -11,9 +11,7 @@ const Chapter = (props) => {
     const {chapters} = useSelector(state => state.chapterState)
     const {chapter_sections} = useSelector(state => state.sectionState)
     const {sections} = useSelector(state => state.chapterState)
-
     const sectionState = useSelector(state => state.sectionState)
-
     // const params = useParams()
     // const {id} = params
     // console.log(props.chapter)
@@ -25,11 +23,11 @@ const Chapter = (props) => {
 
     return (
         <div>
-            <h4 onClick={(e) => {console.log(e); dispatch(handleChapterClick(e, props.chapter))}}>{props.chapter.ordinal_name} {props.chapter.type_one === "front_matter" ? null : `- ${props.chapter.title}`}</h4>
-            <ul>{filteredSections.map(s => <p><a target="_blank" href={`https://scholar.flatworldknowledge.com/books/35880/${s.element_id}`}>{s.ordinal_name}</a></p>)}</ul>
-            {/* <Link to="sections-list" onClick={(e) => {console.log(e); dispatch(handleChapterClick(e, props.chapter))}}>Show details</Link> */}
-            {/* {sectionState.clicked === true ? <Navigate to="chapter_sections"/> : null } */}
+            <h4 onClick={(e) => {e.preventDefault(); dispatch(handleChapterClick(e, props.chapter))}}>{props.chapter.ordinal_name} {props.chapter.type_one === "front_matter" ? null : `- ${props.chapter.title}`}</h4>
+            <ul><a target="_blank" href={`https://scholar.flatworldknowledge.com/books/35880/${props.chapter.element_id}`}>{props.chapter.ordinal_name}</a> - {props.chapter.type_one === "front_matter" ? props.chapter.ordinal_name : props.chapter.title}</ul>
             <SectionsList sections={filteredSections} />
+
+            {/* <Link to="sections-list" onClick={(e) => {console.log(e); dispatch(handleChapterClick(e, props.chapter))}}>Show details</Link> */}
 
         </div>
     )
